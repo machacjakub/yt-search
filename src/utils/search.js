485df455675c923496e7cebcc1fc01c34254224d
jakubmachac <j.machac@post.cz>
2025-05-09 14:36:53 +0200

    refactoring of handleSearch

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,7 +1,7 @@
 import {addToHistory} from "./localStorageUtils.js";
 
 export const handleSearch = (query) => {
-    const encodedQuery = encodeURIComponent(query);
     addToHistory(query)
+    const encodedQuery = encodeURIComponent(query);
     window.location.href = `https://www.youtube.com/results?search_query=${encodedQuery}`;
 };
